b76c68fc0d669810958a9552526b46624ed296e7
Markiyan Pyts <markiyan.pyts@osf.digital>
2022-12-27 12:56:01 +0200

    fix

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,16 +1,16 @@
 import {v4 as uuid} from 'uuid';
 import AWS from 'aws-sdk';
 
-// import middy from '@middy/core';
-// import httpJsonBodyParser from '@middy/http-json-body-parser';
-// import httpEventNormalizer from '@middy/http-event-normalizer';
-// import httpErrorHandler from '@middy/http-error-handler';
-// import createError from 'http-errors';
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
-  const {title} = JSON.parse(event.body)
+  const {title} = event.body
   const now = new Date()
 
   const auction = {
@@ -20,20 +20,15 @@ async function createAuction(event, context) {
     createdAt: now.toISOString()
   };
 
-  await dynamodb.put({
-    TableName: process.env.AUCTIONS_TABLE_NAME,
-    Item: auction
-  }).promise()
-
-  // try {
-  //   await dynamodb.put({
-  //     TableName: process.env.AUCTIONS_TABLE_NAME,
-  //     Item: auction
-  //   }).promise()
-  // } catch(error) {
-  //   console.error(error);
-  //   throw new createError.InternalServerError(error);
-  // }
+  try {
+    await dynamodb.put({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction
+    }).promise()
+  } catch(error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
   
   return {
     statusCode: 201,
@@ -41,4 +36,7 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = createAuction
+export const handler = middy(createAuction)
+.use(httpJsonBodyParser())
+.use(httpEventNormalizer())
+.use(httpErrorHandler());
